42cdef69f74cf1a5a97559e87b6f57dd28ad0e29
shemaprince123 <shemaprince123@gmail.com>
2023-11-20 16:51:31 +0300

    learn c# page content

---
+++ app/src/main/java/com/example/igareg/coursec.java
@@ -0,0 +1,19 @@
+package com.example.igareg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class coursec extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coursec);
+
+
+    }
+}
