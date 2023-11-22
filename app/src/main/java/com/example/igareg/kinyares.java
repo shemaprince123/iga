ffcb816ef47e3d582c926ffa78741dbf0b5e6ac9
shemaprince123 <shemaprince123@gmail.com>
2023-11-22 11:27:10 +0300

    kinya ui done

---
+++ app/src/main/java/com/example/igareg/kinyares.java
@@ -0,0 +1,14 @@
+package com.example.igareg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kinyares extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kinyares);
+    }
+}
---
+++ app/src/main/java/com/example/igareg/kinyaxam.java
@@ -0,0 +1,14 @@
+package com.example.igareg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kinyaxam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kinyaxam);
+    }
+}
