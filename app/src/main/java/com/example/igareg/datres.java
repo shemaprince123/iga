72eaac7c82ab7ac9624d213633399811874bfb80
shemaprince123 <shemaprince123@gmail.com>
2023-11-22 15:26:32 +0300

    All  UI done

---
+++ app/src/main/java/com/example/igareg/datres.java
@@ -0,0 +1,14 @@
+package com.example.igareg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class datres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datres);
+    }
+}
